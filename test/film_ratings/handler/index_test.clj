81b1097c692806347f9e33c409b3281addc7dc14
makutak <takkouno@gmail.com>
2020-05-08 02:42:18 +0900

    レスポンスのテスト追加

--- test/film_ratings/handler/index_test.clj
+++ test/film_ratings/handler/index_test.clj
@@ -1,3 +1,11 @@
 (ns film-ratings.handler.index-test
   (:require [film-ratings.handler.index :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest testing is]]
+            [ring.mock.request :as mock]
+            [integrant.core :as ig]))
+
+(deftest check-index-handler
+  (testing "Ensure that the index handler returns two links for add and list films"
+    (let [handler (ig/init-key :film-ratings.handler/index {})
+          response (handler (mock/request :get "/"))]
+      (is (= :ataraxy.response/ok (first response))))))
