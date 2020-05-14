0f7f72fb81970b1a9ffc5221f86fd0c810926e1b
makutak <takkouno@gmail.com>
2020-05-15 01:10:33 +0900

    Added require modules

--- src/film_ratings/handler/film.clj
+++ src/film_ratings/handler/film.clj
@@ -1,1 +1,6 @@
-(ns film-ratings.handler.film)
+(ns film-ratings.handler.film
+  (:require [ataraxy.core :as ataraxy]
+            [ataraxy.response :as response]
+            [film-ratings.boundary.film :as boundary.film]
+            [film-ratings.views.film :as views.film]
+            [integrant.core :as ig]))
