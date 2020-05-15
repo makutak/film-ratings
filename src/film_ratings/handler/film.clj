72053ff03d79aa69f3bf9217dfeafc306868234a
makutak <takkouno@gmail.com>
2020-05-16 02:54:22 +0900

    Added show create handler

--- src/film_ratings/handler/film.clj
+++ src/film_ratings/handler/film.clj
@@ -4,3 +4,7 @@
             [film-ratings.boundary.film :as boundary.film]
             [film-ratings.views.film :as views.film]
             [integrant.core :as ig]))
+
+(defmethod ig/init-key :film-ratings.handler.film/show-create [_ _]
+  (fn [_]
+    [::response/ok (views.film/create-film-view)]))
