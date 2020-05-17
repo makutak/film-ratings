f7e699e399661954eb22482294651ee1bb97acd1
makutak <takkouno@gmail.com>
2020-05-17 23:34:47 +0900

    Added create handler

--- src/film_ratings/handler/film.clj
+++ src/film_ratings/handler/film.clj
@@ -8,3 +8,14 @@
 (defmethod ig/init-key :film-ratings.handler.film/show-create [_ _]
   (fn [_]
     [::response/ok (views.film/create-film-view)]))
+
+(defmethod ig/init-key :film-ratings.handler.film/create [_ {:keys [db]}]
+  (fn [{[_ film-form] :ataraxy/result :as request}]
+    (let [film (reduce-kv (fn [m k v] (assoc m (keyword k) v))
+                          {}
+                          (dissoc film-form "__anti-forgery-token"))
+          result (boundary.film/create-film db film)
+          alerts (if (:id result)
+                   {:messages ["Film added"]}
+                   result)]
+      [::response/ok (views.film/film-view film alerts)])))
